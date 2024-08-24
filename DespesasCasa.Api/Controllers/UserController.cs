09601d142b5a12ed062260f805baa0a9a67648c6
Arthur Santana <arthurgn.santana@gmail.com>
2024-08-24 12:34:44 -0300

    feat: Add user methods

--- DespesasCasa.Api/Controllers/UserController.cs
+++ DespesasCasa.Api/Controllers/UserController.cs
@@ -1,10 +1,70 @@
+using DespesasCasa.Domain.Interface.Service;
+using DespesasCasa.Domain.Model;
+using DespesasCasa.Domain.Model.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DespesasCasa.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class UserController : ControllerBase
+public class UserController(IUserService _userService) : ControllerBase
 {
+    /// <summary>
+    /// Get User
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    /// <response code="200">Success</response>
+    /// <response code="400">Bad Request</response>
+    /// <response code="500">Internal Server Error</response>
+    [HttpGet("{userId}")]
+    public async Task<IActionResult> GetAsync(Guid userId)
+    {
+        var result = await _userService.GetUserAsync(userId);
+        return Ok(new ResponseViewModel<UserDto>(result));
+    }
 
+    /// <summary>
+    /// Get Users
+    /// </summary>
+    /// <returns></returns>
+    /// <response code="200">Success</response>
+    /// <response code="400">Bad Request</response>
+    /// <response code="500">Internal Server Error</response>
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var result = await _userService.GetAllAsync();
+        return Ok(new ResponseViewModel<List<UserDto>>(result));
+    }
+
+    /// <summary>
+    /// Create User
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    /// <response code="200">Success</response>
+    /// <response code="400">Bad Request</response>
+    /// <response code="500">Internal Server Error</response>
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(UserDto user)
+    {
+        var result = await _userService.CreateAsync(user);
+        return Ok(new ResponseViewModel<Guid>(result));
+    }
+
+    /// <summary>
+    /// Remove User
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    /// <response code="200">Success</response>
+    /// <response code="400">Bad Request</response>
+    /// <response code="500">Internal Server Error</response>
+    [HttpDelete("{userId}")]
+    public async Task<IActionResult> DeleteAsync(Guid userId)
+    {
+        var result = await _userService.DeleteAsync(userId);
+        return Ok(new ResponseViewModel<bool>(result));
+    }
 }
--- DespesasCasa.Domain/Model/Response/ResponseViewModel.cs
+++ DespesasCasa.Domain/Model/Response/ResponseViewModel.cs
@@ -5,7 +5,7 @@ public class ResponseViewModel<T>
     public T? Data { get; set; }
     public long? Count { get; set; }
 
-    public ResponseViewModel(T data, long count)
+    public ResponseViewModel(T data, long count = null)
     {
         Data = data;
         Count = count;
