10ed74e9593ad2ee328b2240b712133fac627c9e
Arthur Santana <arthurgn.santana@gmail.com>
2024-08-27 15:27:19 -0300

    feat: Add CollectionService methods

--- DespesasCasa.Application/Service/CollectionService.cs
+++ DespesasCasa.Application/Service/CollectionService.cs
@@ -1,6 +1,59 @@
+using AutoMapper;
+using DespesasCasa.Domain.Entity;
+using DespesasCasa.Domain.Interface.Repository;
+using DespesasCasa.Domain.Interface.Service;
+using DespesasCasa.Domain.Model.Dto;
+
 namespace DespesasCasa.Application.Service;
 
-public class CollectionService
+public class CollectionService(IUnitOfWork _unitOfWork, IMapper _mapper) : ICollectionService
 {
+    public async Task<CollectionDto> GetCollectionAsync(Guid id)
+    {
+        var collection = await _unitOfWork.CollectionRepository.GetAsync(false, x => x.Id == id);
 
+        if (collection == null)
+        {
+            throw new Exception("Collection not found");
+        }
+
+        return _mapper.Map<CollectionDto>(collection);
+    }
+
+    public async Task<List<CollectionDto>> GetAllAsync()
+    {
+        return _mapper.Map<List<CollectionDto>>(await _unitOfWork.CollectionRepository.GetAllAsync());
+    }
+
+    public async Task<Guid> CreateAsync(CollectionDto collection)
+    {
+        var existing = await _unitOfWork.CollectionRepository.FindAsync(x => x.Id == collection.Id);
+
+        if (existing)
+        {
+            throw new Exception("Collection already exists");
+        }
+
+        var newCollection = _mapper.Map<Collection>(collection);
+
+        await _unitOfWork.CollectionRepository.AddAsync(newCollection);
+        await _unitOfWork.CommitAsync();
+
+        return newCollection.Id;
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var collection = await _unitOfWork.CollectionRepository.GetAsync(false, x => x.Id == id);
+
+        if (collection is null)
+        {
+            throw new Exception("Collection not found");
+        }
+
+        _unitOfWork.CollectionRepository.Remove(collection);
+        await _unitOfWork.CommitAsync();
+
+        return true;
+    }
 }
