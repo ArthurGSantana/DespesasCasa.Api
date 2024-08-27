59ef60d7c8316f74d2727584991d9e563904f6bd
Arthur Santana <arthurgn.santana@gmail.com>
2024-08-27 15:39:36 -0300

    feat: Implement ExpenseService methods for CRUD operations

--- DespesasCasa.Application/Service/ExpenseService.cs
+++ DespesasCasa.Application/Service/ExpenseService.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using DespesasCasa.Domain.Entity;
 using DespesasCasa.Domain.Interface.Repository;
 using DespesasCasa.Domain.Interface.Service;
 using DespesasCasa.Domain.Model.Dto;
@@ -21,22 +22,56 @@ public class ExpenseService(IUnitOfWork _unitOfWork, IMapper _mapper) : IExpense
 
     public async Task<List<ExpenseDto>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        return _mapper.Map<List<ExpenseDto>>(await _unitOfWork.ExpenseRepository.GetAllAsync());
     }
 
     public async Task<Guid> CreateAsync(ExpenseDto expense)
     {
-        throw new NotImplementedException();
+        var existing = await _unitOfWork.ExpenseRepository.FindAsync(x => x.Id == expense.Id);
+
+        if (existing)
+        {
+            throw new Exception("Expense already exists");
+        }
+
+        var newExpense = _mapper.Map<Expense>(expense);
+
+        await _unitOfWork.ExpenseRepository.AddAsync(newExpense);
+        await _unitOfWork.CommitAsync();
+
+        return newExpense.Id;
     }
 
     public async Task<bool> UpdateAsync(ExpenseDto expense)
     {
-        throw new NotImplementedException();
+        var existing = await _unitOfWork.ExpenseRepository.FindAsync(x => x.Id == expense.Id);
+
+        if (!existing)
+        {
+            throw new Exception("Expense not found");
+        }
+
+        var updatedExpense = _mapper.Map<Expense>(expense);
+
+        _unitOfWork.ExpenseRepository.Update(updatedExpense);
+        await _unitOfWork.CommitAsync();
+
+        return true;
     }
 
     public async Task<bool> DeleteAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var expense = await _unitOfWork.ExpenseRepository.GetAsync(false, x => x.Id == id);
+
+        if (expense is null)
+        {
+            throw new Exception("Expense not found");
+        }
+
+        _unitOfWork.ExpenseRepository.Remove(expense);
+        await _unitOfWork.CommitAsync();
+
+        return true;
     }
 
 }
