9f9199e1cf793bb805cd3d785b97751ddb5bc38c
Arthur Santana <arthurgn.santana@gmail.com>
2024-08-28 19:16:27 -0300

    feat: Add string extension methods

--- DespesasCasa.Domain/Extensions/StringExtensions.cs
+++ DespesasCasa.Domain/Extensions/StringExtensions.cs
@@ -1,8 +1,78 @@
 using System;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace DespesasCasa.Domain.Extensions;
 
-public class StringExtensions
+public static class StringExtensions
 {
+    public static string? ToSnakeCase(this string? text)
+    {
+        if (text is null)
+        {
+            throw new ArgumentNullException(nameof(text));
+        }
 
+        if (text.Length < 2)
+        {
+            return text;
+        }
+
+        var sb = new StringBuilder();
+        sb.Append(char.ToLowerInvariant(text[0]));
+        for (int i = 1; i < text.Length; ++i)
+        {
+            char c = text[i];
+            if (char.IsUpper(c))
+            {
+                sb.Append('_');
+                sb.Append(char.ToLowerInvariant(c));
+            }
+            else
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    public static string? TrimAll(this string? text)
+    {
+        if (text is null)
+        {
+            throw new ArgumentNullException(nameof(text));
+        }
+
+        return Regex.Replace(text, @"\s+", "_", RegexOptions.None, TimeSpan.FromSeconds(5));
+    }
+
+    public static string RemoveDiacritics(this string text)
+    {
+        var normalizedString = text.Normalize(NormalizationForm.FormD);
+        var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
+
+        for (int i = 0; i < normalizedString.Length; i++)
+        {
+            char c = normalizedString[i];
+            var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+            if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+            {
+                stringBuilder.Append(c);
+            }
+        }
+
+        return stringBuilder
+            .ToString()
+            .Normalize(NormalizationForm.FormC);
+    }
+
+    public static string TrimMultipleWhitespaces(this string text)
+    {
+        RegexOptions options = RegexOptions.None;
+        Regex regex = new Regex("[ ]{2,}", options, TimeSpan.FromMilliseconds(1000));
+        text = regex.Replace(text, " ");
+        return text.Trim();
+    }
 }
