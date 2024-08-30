7fc5b1cb0b36b7804caa6428875be2e9d4904032
Arthur Santana <arthurgn.santana@gmail.com>
2024-08-30 15:27:29 -0300

    chore: Update JWT configuration and dependencies for authentication

--- DespesasCasa.Api/Program.cs
+++ DespesasCasa.Api/Program.cs
@@ -1,8 +1,11 @@
+using System.Text;
 using System.Text.Json.Serialization;
 using DespesasCasa.Api.Environment;
 using DespesasCasa.Api.Filters;
 using DespesasCasa.Domain.Config;
 using DespesasCasa.IoC;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 const string allowedOriginsName = "_allowedOrigins";
 
@@ -23,6 +26,23 @@ builder.Services.AddControllers(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? ""))
+            };
+        }
+    );
 
 builder.Services.Configure<ApplicationConfig>(configuration);
 
